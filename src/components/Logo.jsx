e67a7adf57c3c2bfb84e5fb6f2d0dab32cc94ca9
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-26 15:38:24 -0600

    Logo component and style finished

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,20 @@
 import LogoImage from '../assets/Logo.png'
 
+import '../styles/Logo.scss'
 
 function Logo() {
   return (
-    <div>
+    <div className='logo-container'>
       <figure>
-        <img src={LogoImage} alt='Logo' width={50} />
-        <p>learn</p>
+        <img
+          className='logo-image' 
+          src={LogoImage}
+          alt='Logo'
+          width={50}
+        />
+        <figcaption className='logo-text'>learn</figcaption>
       </figure>
+      
     </div>
   )
 }
