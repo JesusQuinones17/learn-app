7627afe0c00a40120f083146e5e2882a4e72d16f
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-30 17:16:12 -0600

    Login form created and added to Login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Button from "./Button";
+
+import "../styles/LoginForm.scss";
 
 function LoginForm() {
+  const handleSubmmitButton = () => {
+    handleSubmmitForm;
+  };
+
+  const handleSubmmitForm = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form className="form-login" onSubmit={handleSubmmitForm}>
       <label>
         User name
         <input type="text" placeholder="Enter email" />
       </label>
+      <label>
+        Password
+        <input type="password" placeholder="Enter password" />
+      </label>
+      <Button className="" buttonText="Sign in" onClick={handleSubmmitButton} />
     </form>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
+import LoginForm from "../components/LoginForm";
+
 function Login() {
-    return (
-      <article>
-          <h1>Hello from Login</h1>
-      </article>
-    )
-  }
-  
-  export default Login
-  
+  return (
+    <article>
+      <h1>Hello from Login</h1>
+      <LoginForm />
+    </article>
+  );
+}
+
+export default Login;
