fc4f80e6e66c6d9325876152a0b2cb15d37721c1
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-30 10:23:51 -0600

    Prop className added to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,8 @@
-function Button({}) {
-  return <button></button>;
+function Button({ className, buttonText, onClick }) {
+  return (
+    <button className={className} onClick={onClick}>
+      {buttonText}
+    </button>
+  );
 }
 export default Button;
