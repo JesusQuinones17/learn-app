147049ddb91537d1403882eb4156cdff23eb9d6b
JesusQuinones17 <jesus_quinones@epam.com>
2023-10-30 22:58:27 -0600

    Login form finished and almost Login component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -14,15 +14,32 @@ function LoginForm() {
 
   return (
     <form className="form-login" onSubmit={handleSubmmitForm}>
-      <label>
-        User name
-        <input type="text" placeholder="Enter email" />
-      </label>
-      <label>
-        Password
-        <input type="password" placeholder="Enter password" />
-      </label>
-      <Button className="" buttonText="Sign in" onClick={handleSubmmitButton} />
+      <div className="form-inputs-container">
+        <label htmlFor="email" className="form-login-label">
+          User name
+          <input
+            id="email"
+            className="form-login-input"
+            type="text"
+            placeholder="Enter email"
+            autoComplete="on"
+          />
+        </label>
+        <label htmlFor="password" className="form-login-label">
+          Password
+          <input
+            id="password"
+            className="form-login-input"
+            type="password"
+            placeholder="Enter password"
+          />
+        </label>
+      </div>
+      <Button
+        className="form-login-button"
+        buttonText="Sign in"
+        onClick={handleSubmmitButton}
+      />
     </form>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
+
 import LoginForm from "../components/LoginForm";
+import Header from "../components/Header";
+
+import "../styles/Login.scss";
 
 function Login() {
   return (
-    <article>
-      <h1>Hello from Login</h1>
-      <LoginForm />
-    </article>
+    <>
+      <Header />
+      <article className="login">
+        <h1 className="login-title">Sign In</h1>
+        <h2 className="login-text">Welcome back</h2>
+        <LoginForm />
+        <p className="login-text">
+          Don't have an account?{" "}
+          <Link to={"/registration"}>
+            <strong>Sign up</strong>
+          </Link>
+        </p>
+      </article>
+    </>
   );
 }
 
