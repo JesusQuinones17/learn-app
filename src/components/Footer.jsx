9aa12e6740ba614edd19954b2151956439c64d19
JesusQuinones17 <jesus_quinones@epam.com>
2023-11-01 11:21:25 -0600

    Footer finished

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+import Logo from "../components/Logo";
+import Button from "../components/Button";
+
+import "../styles/Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-container-col">
+          <Logo></Logo>
+        </div>
+        <div className="footer-container-col">
+          <h4>Product</h4>
+          <ul>
+            <li>
+              <Link to={"/features"}>Features</Link>
+            </li>
+            <li>
+              <Link to={"/pricing"}>Pricing</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-container-col">
+          <h4>Resources</h4>
+          <ul>
+            <li>
+              <Link to={"/blog"}>Blog</Link>
+            </li>
+            <li>
+              <Link to={"/webinars"}>Webinars</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-container-col">
+          <h4>Company</h4>
+          <ul>
+            <li>
+              <Link to={"/about"}>About us</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-container-col">
+          <h4>Subscribe to our newsletter</h4>
+          <div className="footer-col">
+            <form>
+              <label htmlFor="newsletter">
+                <input id="newsletter" type="text" />
+              </label>
+              <Button
+                className={"footer-container-button"}
+                buttonText={"Subscribe"}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
