fc0bd9bad3b73a2cbdc12a11544b852280ddc007
JesusQuinones17 <jesus_quinones@epam.com>
2023-11-08 10:38:32 -0600

    Home page finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,47 @@
+import { useNavigate } from "react-router-dom";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
+
+import HomeVideoImage from "../assets/MainImage.png";
 
 import "../styles/Home.scss";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <article>
       <Header />
+      <main className="home">
+        <article className="home-container">
+          <h1>Let's start learning</h1>
+          <p className="home-message">
+            Welcome to Learn Platform - where every day is a day to learn. Dive
+            into the vast ocean of knowledge and empower yourself with the tools
+            for a successful tomorrow. Happy learning!
+          </p>
+        </article>
+        <figure className="home-image">
+          <img
+            className="home-video-image"
+            src={HomeVideoImage}
+            alt="VideoImage"
+          />
+        </figure>
+        <article className="home-join-us-container">
+          <h2 className="home-join-us-title">Join us</h2>
+          <p className="home-join-us-message">
+            Qui ut exercitation officia proident enim non tempor tempor ipsum ex
+            nulla ea adipiscing sit consequat enim elit cupidatat o
+          </p>
+          <Button
+            buttonText={"Join us"}
+            className={"home-join-us-button"}
+            onClick={() => navigate("/join")}
+          />
+        </article>
+      </main>
       <Footer />
     </article>
   );
